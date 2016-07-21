b1ceebf1ff7ee384729a78726810a0052f2c8eda
NoahOliverRigonan <oliverrigonan@gmail.com>
2016-07-21 10:48:11 +0800

    Lending Commit: First Commit

---
+++ Lending/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lending.MvcApplication" Language="C#" %>
---
+++ Lending/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lending.Startup))]
+namespace Lending
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
